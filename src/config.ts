ba72065ef151dec39b1c26d49414b5c45c7e1764
wutangyanmai <wutangyanmai@gmail.com>
2025-04-27 07:43:04 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,11 @@ export let PORT = 377; // 端口
 
 export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [
+  {
+    domain: string; // 域名
+    url: string; // 网址
+    enable: boolean; // 开启
+    template?: Template; // 模板
+  }
+];
