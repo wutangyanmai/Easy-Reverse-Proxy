6dd3bf6c9eb5c128c45d1ff8150933f1be5b7970
wutangyanmai <wutangyanmai@gmail.com>
2025-04-27 08:52:07 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
-export interface Proxy {
-    domain: string;
-    url: string;
-}
+import type {Proxy} from './util/model';
 
+export let PORT = 377; // 端口
+
+export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
+// 代理
 export let PROXIES: Proxy[] = [
   {
-    domain: "easy-reverse-proxy-ecru.vercel.app",
-    url: "https://www.google.com"
+        domain: "www.yikoc.com",
+        url: "https://google.com/",
+        enable: true
   }
 ];
