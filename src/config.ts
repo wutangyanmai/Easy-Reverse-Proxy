1f9e24ff6662b99929841af9808f5ab0ce5a746c
wutangyanmai <wutangyanmai@gmail.com>
2025-04-27 08:44:22 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,11 @@
-import type {Proxy} from './util/model';
+export interface Proxy {
+    domain: string;
+    url: string;
+}
 
-export let PORT = 377; // 端口
-
-export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
-// 代理
 export let PROXIES: Proxy[] = [
   {
-    domain: 'easy-reverse-proxy-ecru.vercel.app'; // 域名
-    url: 'https://www.bing.com'; // 网址
-    enable: boolean; // 开启
-    template?: Template; // 模板
+    domain: "easy-reverse-proxy-ecru.vercel.app",
+    url: "https://www.bing.com"
   }
 ];
