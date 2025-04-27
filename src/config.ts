8adb339f31efb8576b9502eab66e8e1f182ab091
wutangyanmai <wutangyanmai@gmail.com>
2025-04-27 08:38:01 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [
   {
-    domain: "easy-reverse-proxy-ecru.vercel.app"; // 域名
-    url: "https://www.bing.com"; // 网址
+    domain: 'easy-reverse-proxy-ecru.vercel.app'; // 域名
+    url: 'https://www.bing.com'; // 网址
     enable: boolean; // 开启
     template?: Template; // 模板
   }
