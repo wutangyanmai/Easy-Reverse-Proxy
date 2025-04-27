ccd160a859a7be40e5f925375661ddd8982c94a4
wutangyanmai <wutangyanmai@gmail.com>
2025-04-27 08:57:41 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [
   {
-        domain: "www.yikoc.com",
+        domain: "easy-reverse-proxy-chi.vercel.app",
         url: "https://google.com/",
         enable: true
   }
